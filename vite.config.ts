fd5d507f3ca4bc25136e87ee9124c7bb83cf5e7f
interniums <intuition228@gmail.com>
2024-11-13 09:09:09 +0200

    fixed rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
